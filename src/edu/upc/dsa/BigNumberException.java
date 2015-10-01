02056a0a04a2eff03d6402630b898862933b468f
tono <tonollado@hotmail.com>
2015-10-01 20:16:46 +0200

    Initial commit

---
+++ src/edu/upc/dsa/BigNumberException.java
@@ -0,0 +1,10 @@
+package edu.upc.dsa;
+
+/**
+ * Created by tono on 01/10/2015.
+ */
+public class BigNumberException extends Exception {
+    public BigNumberException(String message) {
+        super(message);
+    }
+}
---
+++ src/edu/upc/dsa/FileParsingException.java
@@ -0,0 +1,10 @@
+package edu.upc.dsa;
+
+/**
+ * Created by tono on 01/10/2015.
+ */
+public class FileParsingException extends Exception {
+    public FileParsingException(String message) {
+        super(message);
+    }
+}
